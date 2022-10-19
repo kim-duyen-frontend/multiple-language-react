24505f51fdc5e871e10f705426ff32355b2e0afb
kim-duyen-frontend <ntduyen255@gmail.com>
2022-10-19 13:50:37 +0700

    commit da ngon ngu voi i18next

--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
+import React from 'react';
+import { useTranslation, Trans } from 'react-i18next';
 import logo from './logo.svg';
 import './App.css';
 
+const languages = {
+  en: { nativeName: 'English' },
+  vi: { nativeName: 'Vietnamese' }
+};
 function App() {
+  const { i18n } = useTranslation();
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <div>
+          {Object.keys(languages).map((lng) => (
+            <button className={`language ${i18n.resolvedLanguage === lng ? "active" : ""}`} key={lng} type="submit" onClick={() => i18n.changeLanguage(lng)}>
+              {languages[lng].nativeName}
+            </button>
+          ))}
+        </div>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <Trans i18nKey="description.part1">
+            Edit <code>src/App.js</code> and save to reload.
+          </Trans>
         </p>
         <a
           className="App-link"
@@ -15,7 +32,9 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          <Trans i18nKey="description.part2">
+            Learn React
+          </Trans>
         </a>
       </header>
     </div>
